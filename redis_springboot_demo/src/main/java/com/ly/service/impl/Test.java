ce7a6bb4b8d8c3683e8e939460ef01afc65520a9
zx11492 <xu6.zhang@ly.com>
2023-12-15 16:17:02 +0800

    版本X

--- redis_springboot_demo/src/main/java/com/ly/service/impl/Test.java
+++ redis_springboot_demo/src/main/java/com/ly/service/impl/Test.java
@@ -1,16 +1,10 @@
 package com.ly.service.impl;
 
-import com.google.common.base.Splitter;
-import freemarker.template.utility.DateUtil;
-import kotlin.text.Charsets;
 import lombok.extern.slf4j.Slf4j;
-import org.checkerframework.checker.units.qual.C;
 
-import java.net.URLDecoder;
-import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * ClassName: Test
@@ -27,50 +21,58 @@ public class Test {
 
 //        String[] arr = "epc,epcindex,eh5,tapphotel,eclockhotel,th5,yunshanfu,yunshanfuindex,addition,i-addition,tappaddition,tpc,tpcindex,baiduxiaochengxu,kuaiyingyong,toutiao,toutiaoindex,baiduxiaochengxuindex,kuaiyingyongindex,tappclockhotel,th5clockhotel,baiduxiaochengxuclockhotel,toutiaoclockhotel,kuaishou,kuaishouindex,dianjinghotel,qipaihotel,leisurehotel,leisurehotelindex,tleisurehotel,tappleisurehotel,douyin_lite,i-epc,i-epcindex,i-tpc,i-tpcindex,i-eh5,i-th5,i-baiduxiaochengxu,i-toutiao,i-kuaishou,i-douyin_lite,i-tgopc,toutiaoxcx,douyinxcx,toutiaoclockhotelxcx,douyinclockhotel,i-douyin,toutiao_lite,i-toutiao_lite,eminsu,tminsu,tappminsu,alipayxcx,alipayclockhotelxcx,alipayminsuxcx,alipayxcxindex,epchome,i-epchome,subwayxcx,subwayclockhotelxcx,subwayminsuxcx,i-subwayxcx,wxplugin,wxclockhotelplugin,wxminsuplugin,wxxcx,wxclockhotelxcx,wxminsuxcx,i-tapphotel,kuaiyingyongminsu,kuaiyingyongclockhotel,i-kuaiyingyong,i-alipayxcx,ikuaiyingyongindex,kuaiyingyongclockhotelindex,kuaiyingyongminsuindex,i-yunshanfu,alipayaddition,huixing".split(",");
 //        String[] index = "eclockhotel,kuaiyingyong,i-kuaiyingyong,kuaiyingyongminsu,kuaiyingyongclockhotel,subwayxcx,alipayxcxindex,alipayxcx,alipayclockhotelxcx,alipayminsuxcx,tapphotel,tappclockhotel,tappminsu,i-tapphotel,eminsu,tminsu,subwayminsuxcx,wxminsuplugin,wxminsuxcx".split(",");
-        String cnUserValue = "userid=454116888&nickName=kissBaiCha&loginType=&token=203039007222250079029251141152081104137007081099038184122148137063086035091239211025013027087224158179018084075171109156216059178255106038018083098117007188090037179015137210057093003165082079167195154250047064086077023057017044170070000044161128034179157071255068205182183225230147070191033185014232231140106181&authcode=13524536F32DCE87A19835EF2BC517BC&secsign=88HnDXC7-ksml8NHzFgMI0xhMgK4fpiZsOImNOHThaPSKJzlqhHgUb2MH7ffRPB_Wd";
+        String cnUserValue = "userid=1048732781&token=029200194120060239163017177246195236110107108221042028161090033060033085165180181239119055229176165137006189195199083084075190150121045087090063140125184244014050052084017242045104095157219174145100050172249109151127&loginType=passport&secsign=81DRqbkHAeTsSlvSt6ub4QNiEEqBAfQMmcaSed4vkUGkVRmhJu2PKPt-BD20YtG9l5&authcode=B71DDDAFC6A2D0BE3F90E69DF40A3957";
 
-        log.info(subEncryptMemberIdForSecsign(cnUserValue,"token="));
-        Date date = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        String format = formatter.format(date);
-        System.out.println(format);
+        log.info(getSubCookiePram(cnUserValue,"secsign="));
 
     }
 
-    private static String subEncryptMemberIdForSecsign(String cnUserValue,String cookiePram) {
-
-        int startIndex = cnUserValue.indexOf(cookiePram) + cookiePram.length();
+    public static String getSubCookiePram(String cookieValue,String cookiePram) {
+        int startIndex = cookieValue.indexOf(cookiePram) + cookiePram.length();
         String result = "";
         if (startIndex >= 0) {
-            if (cnUserValue.indexOf("&", startIndex) >= 0) {
-                result = cnUserValue.substring(startIndex, cnUserValue.indexOf("&", startIndex));
+            if (cookieValue.indexOf("&", startIndex) >= 0) {
+                result = cookieValue.substring(startIndex, cookieValue.indexOf("&", startIndex));
             } else {
-                result = cnUserValue.substring(startIndex);
+                result = cookieValue.substring(startIndex);
             }
         }
         return result;
     }
+    // 方法接收集合数据和筛选条件的 Map
+    public static <T> List<T> filterCollection(List<T> collection, Map<String, Object> filterMap) {
+        List<T> filteredList = new ArrayList<>();
 
+        for (T item : collection) {
+            boolean isMatched = true;
 
-    public static String getOneParameter(String url,String keyWord) {
-        String retValue = "";
-        try {
-            final String charset = "utf-8";
-            url = URLDecoder.decode(url, charset);
-            if (url.indexOf('?') != -1) {
-                final String contents = url.substring(url.indexOf('?') + 1);
-                String[] keyValues = contents.split("&");
-                for (String keyValue : keyValues) {
-                    String key = keyValue.substring(0, keyValue.indexOf("="));
-                    String value = keyValue.substring(keyValue.indexOf("=") + 1);
-                    if (key.equals(keyWord)) {
-                        retValue = value;
+            for (Map.Entry<String, Object> entry : filterMap.entrySet()) {
+                try {
+                    // 获取实体对象的属性值
+                    Object propertyValue = item.getClass().getMethod("get" + capitalize(entry.getKey())).invoke(item);
+
+                    // 检查实体对象的属性值是否符合筛选条件
+                    if (!entry.getValue().equals(propertyValue)) {
+                        isMatched = false;
+                        break;
                     }
+                } catch (Exception e) {
+                    // 处理异常情况，可以根据实际情况进行处理或记录日志
+                    isMatched = false;
+                    break;
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+            if (isMatched) {
+                filteredList.add(item);
+            }
         }
-        return retValue;
+
+        return filteredList;
+    }
+
+    // 辅助方法：将属性名的首字母大写，以便构造 getter 方法名
+    private static String capitalize(String str) {
+        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
     }
 }
